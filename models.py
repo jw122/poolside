8b0c3bbac0411f590a53e187da228ed1cda06fac
James Levy <jamesalexanderlevy@gmail.com>
2021-01-22 12:29:38 -0800

    formatting

--- models.py
+++ models.py
@@ -6,7 +6,20 @@ class Model():
     def to_dict(self):
        return dict([(p, unicode(getattr(self, '%s_to_dict' % p, getattr(self, p)))) for p in self.properties()])
 
-class Token(db.Expando, Model):
+class TokenModel(Model):
+
+    @property
+    def tradeVolume_to_dict(self):
+        if self.tradeVolume > 1000000:
+            return '%sM' %  math.floor(self.tradeVolume/1000000)
+        if self.tradeVolume > 10000:
+            return '%sk' %  math.floor(self.tradeVolume/1000)
+        elif self.tradeVolume:
+            return '%s' % self.tradeVolume
+        else:
+            return ''
+
+class Token(db.Expando, TokenModel):
     # key_name is id
     id = db.StringProperty()
     symbol = db.StringProperty(required=False)
@@ -20,16 +33,9 @@ class Token(db.Expando, Model):
     modified = db.DateTimeProperty(auto_now=True)
 
 
-    @property
-    def tradeVolume_to_dict(self):
-        if self.tradeVolume > 10000:
-            return '%sk' %  math.floor(self.tradeVolume/1000)
-        elif self.tradeVolume:
-            return '%s' % self.tradeVolume
-        else:
-            return ''
 
-class Pair(db.Expando, Model):
+
+class Pair(db.Expando, TokenModel):
     # key_name is id
     id = db.StringProperty()
     symbol = db.StringProperty(required=False)
